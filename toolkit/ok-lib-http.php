75e1a3714b83c2c80692c0954df8e763f144ea7f
mike <mikecaines@gmail.com>
2015-01-11 14:55:25 -0400

    createHttpStatusHeader() which formats an HTTP 1.x status header string from an int status code

---
+++ toolkit/ok-lib-http.php
@@ -0,0 +1,32 @@
+<?php 
+function ok_createHttpStatusHeader($aHttpStatusCode) {
+	$headers = array(
+		200 => '200 OK',
+		301 => '301 Moved Permanently',
+		304 => '304 Not Modified',
+		307 => '307 Temporary Redirect',
+		403 => '403 Forbidden',
+		404 => '404 Not Found',
+		408 => '408 Request Time-out',
+		500 => '500 Internal Server Error',
+		501 => '501 Not Implemented',
+		503 => '503 Service Unavailable',
+	);
+
+	if (!array_key_exists($aHttpStatusCode, $headers)) {
+		throw new Exception("Cannot create HTTP status header with code '" . $aHttpStatusCode . "'.");
+	}
+
+	$protocol = null;
+	if (isset($_SERVER) && array_key_exists('SERVER_PROTOCOL', $_SERVER) && stripos($_SERVER['SERVER_PROTOCOL'], 'HTTP') !== false) {
+		$protocol = $_SERVER['SERVER_PROTOCOL'];
+	}
+
+	if (!$protocol) {
+		throw new Exception("Cannot create HTTP status header because the protocol cannot be determined.");
+	}
+
+	$header = $protocol . ' ' . $headers[$aHttpStatusCode];
+
+	return $header;
+}
