d5a4e9a20532c9c1cc3a32cc96526652cf04170b
mike <mikecaines@gmail.com>
2015-01-11 15:05:13 -0400

    createSlug()

---
+++ toolkit/ok-lib-slug.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Creates a URL slug.
+ * i.e. Creates "foo-bar" from "Foo Bar" for use in "http://host.com/foo-bar/".
+ * @param string $aText Text to be slugified.
+ * @param array $aOptions Additional options.
+ * @return string Slug.
+ */
+function ok_createSlug($aText, $aOptions = array()) {
+	$options = array_merge(array(
+		'preserveCase' => false,
+	), $aOptions);
+	
+	$text = $aText;
+	
+	if ($options['preserveCase'] == false) {
+		$text = strtolower($text);
+	}
+	
+	$text = str_replace(' ', '-', $text);
+	$text = preg_replace('/[^a-z0-9\-]/i', '', $text);
+	$text = preg_replace('/-{2,}/', '-', $text);
+	$text = preg_replace('/(^-)|(-$)/', '', $text);
+
+	return $text;
+}
