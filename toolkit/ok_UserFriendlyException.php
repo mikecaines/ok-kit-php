53579515d42097821ede6a18eac3b61bf08076bc
mike <mikecaines@gmail.com>
2015-03-21 10:40:18 -0300

    remove some exception classes which are not generic enough for ok-kit

--- toolkit/ok_UserFriendlyException.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'ok_UserFriendlyExceptionInterface.php';
-
-class ok_UserFriendlyException extends Exception implements ok_UserFriendlyExceptionInterface {
-    public function getUserFriendlyMessage() {
-        return $this->getMessage();
-    }
-}
--- toolkit/ok_UserFriendlyExceptionInterface.php
+++
@@ -1,4 +0,0 @@
-<?php
-interface ok_UserFriendlyExceptionInterface {
-    function getUserFriendlyMessage();
-}
