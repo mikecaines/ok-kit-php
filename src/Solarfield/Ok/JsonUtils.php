7f6d6326f44b7b7ab2515a05ee5cf89743300f57
mike <mikecaines@gmail.com>
2016-01-23 19:06:15 -0400

    JsonUtils::parse() which parses to associative array and throws exception if parsing failed

--- src/Solarfield/Ok/JsonUtils.php
+++ src/Solarfield/Ok/JsonUtils.php
@@ -2,6 +2,18 @@
 namespace Solarfield\Ok;
 
 abstract class JsonUtils {
+	static public function parse($aText) {
+		$json = json_decode($aText, true);
+
+		if (($error = json_last_error()) != JSON_ERROR_NONE) {
+			throw new \Exception(
+				"Parsing JSON failed with code $error."
+			);
+		}
+
+		return $json;
+	}
+
 	static public function toJson($aThing) {
 		if (
 			(is_object($aThing) && $aThing instanceof ToArrayInterface)
