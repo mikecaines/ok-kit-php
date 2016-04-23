2b1a8100ecce584bebf0f4da3d86873d0127d30c
mike <mikecaines@gmail.com>
2016-04-23 10:48:10 -0300

    StructProxy class

---
+++ src/Solarfield/Ok/StructProxy.php
@@ -0,0 +1,54 @@
+<?php
+namespace Solarfield\Ok;
+
+
+class StructProxy {
+	private $data;
+
+	public function &getData() {
+		return $this->data;
+	}
+	
+	public function get($aPath) {
+		return StructUtils::get($this->data, $aPath);
+	}
+	
+	public function getAsString($aPath) {
+		return (string)$this->get($aPath);
+	}
+
+	public function getAsArray($aPath) {
+		$value = $this->get($aPath);
+
+		if (is_array($value) || $value instanceof ToArrayInterface) {
+			return $value;
+		}
+
+		$arr = [];
+
+		if (is_object($value)) {
+			foreach ($value as $k => $v) {
+				$arr[$k] = $v;
+			}
+		}
+
+		return $arr;
+	}
+
+	public function getAsBool($aPath) {
+		return (bool)$this->get($aPath);
+	}
+
+	public function set($aPath, $aValue) {
+		StructUtils::set($this->data, $aPath, $aValue);
+	}
+
+	public function __construct(array &$aData = null) {
+		if ($aData != null) {
+			$this->data = &$aData;
+		}
+		else {
+			$this->data = [];
+		}
+	}
+}
